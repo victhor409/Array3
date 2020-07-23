c5c2a84badd2222f3a6a1dbae53d732be453c024
victhor409 <victhor409@gmail.com>
2020-07-22 23:16:53 -0300

    Array Ex 3

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,21 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Produto;
+/*
+ * A dona de um pensionato possui dez quartos para alugar para estudantes,
+sendo esses quartos identificados pelos números 0 a 9.
+Fazer um programa que inicie com todos os dez quartos vazios, e depois
+leia uma quantidade N representando o número de estudantes que vão
+alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+N estudantes. Para cada registro de aluguel, informar o nome e email do
+estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+um relatório de todas ocupações do pensionato, por ordem de quarto,
+conforme exemplo.
+ */
+
+
+import entities.Rooms;
 
 public class Program {
 
@@ -11,31 +25,36 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in); 
 		
+		System.out.print("How many rooms will be rented?: ");
 		int n = sc.nextInt();
+		System.out.println();
 		
-		Produto[] vect = new Produto[n];
 		
-		for(int i=0; i<n; i++) {
+		Rooms[] vect = new Rooms[10];
+		
+		for(int i=1; i<=n; i++) {
 			
-			String nome=sc.next();
-			double preco=sc.nextDouble();
+			System.out.println("Rent #"+i);
+			System.out.print("Name: ");
+			String name=sc.next();
+			sc.nextLine();
+			System.out.print("Email: ");
+			String mail=sc.next();
+			System.out.print("Room: ");
+			int room=sc.nextInt();
+			System.out.println();
 			
-			vect[i]= new Produto(nome,preco);
+			vect[i]=new Rooms(name,mail,room);
+			
 		}
 		
+		System.out.println("Busy Rooms");
 		
-		double sum=0.0;
 		for(int i=0; i<vect.length; i++) {
-			
-			sum += vect[i].getPreco();
-			
+			if (vect[i] != null) {
+				System.out.println(vect[i]);
+			}
 		}
-		
-		double avg = sum/n;
-		
-		System.out.printf("AVG: %.2f%n",avg);
-		
-		
 		
 		sc.close();
 	
---
+++ src/entities/Rooms.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class Rooms {
+	
+	private String name;
+	private String mail;
+	private int room;
+	
+	public Rooms() {
+		
+	}
+
+	public Rooms(String name, String mail, int room) {
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.room = room;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getRoom() {
+		return room;
+	}
+
+	public void setRoom(int room) {
+		this.room = room;
+	}
+
+	@Override
+	public String toString() {
+		return room + ": " + name + ", " + mail;
+	}
+	
+	
+
+}
